6c7333945b5a8de09004d56ecfd263b17da1eb7c
Tommi Cox <tommicox@gmail.com>
2019-03-07 17:55:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-the_manifest[oil paintings]
+shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -37,13 +37,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+the_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+the_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+the_manifest
 end
