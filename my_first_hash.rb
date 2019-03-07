a8d7a7994f5c0a552d9e76aabac21fce9c768fc8
Tommi Cox <tommicox@gmail.com>
2019-03-07 17:41:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,3 @@
-
-
 def my_hash
  
   
@@ -8,7 +6,7 @@ end
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
-end
+
 
 
 def shipping_manifest
